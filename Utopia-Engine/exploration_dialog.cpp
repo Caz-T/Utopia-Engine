16e27a67f4c0134bfe7ae160d2da5bc2e37045bc
Caz-T <89822413+Caz-T@users.noreply.github.com>
2022-06-03 22:39:59 +0800

    implemented the exploration_dialog!

--- Utopia-Engine/exploration_dialog.cpp
+++ Utopia-Engine/exploration_dialog.cpp
@@ -1,14 +1,157 @@
 #include "exploration_dialog.h"
 #include "ui_exploration_dialog.h"
 
-exploration_dialog::exploration_dialog(QWidget *parent) :
+#include <QMessageBox>
+
+exploration_dialog::exploration_dialog(game_controller* gm, int id, QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::exploration_dialog)
+    ui(new Ui::exploration_dialog),
+    game(gm),
+    d6(new dice),
+    location_id(id)
 {
     ui->setupUi(this);
+
+    blanks[0] = ui->label;
+    blanks[1] = ui->label_2;
+    blanks[2] = ui->label_3;
+    blanks[3] = ui->label_4;
+    blanks[4] = ui->label_5;
+    blanks[5] = ui->label_6;
+    buttons[0] = ui->pushButton;
+    buttons[1] = ui->pushButton_2;
+    buttons[2] = ui->pushButton_3;
+    buttons[3] = ui->pushButton_4;
+    buttons[4] = ui->pushButton_5;
+    buttons[5] = ui->pushButton_6;
+
+    for (auto& button : buttons) button->setVisible(false);
+
+    QString envi_text = "当前环境效果：\n";
+    if (game->location_event(location_id) == 3) envi_text.append("好运气——搜索结果-10\n");
+    if (game->artifact_status(1) == 2 and (location_id == 0 or location_id == 5)) envi_text.append("隐士之镜——此处搜索结果-10\n");
+    if (game->artifact_status(4) == 2 and (location_id == 2 or location_id == 3)) envi_text.append("预示棱镜——此处搜索结果-10\n");
+    ui->envi_effects->setText(envi_text);
+
 }
 
 exploration_dialog::~exploration_dialog()
 {
+    delete d6;
     delete ui;
 }
+
+void exploration_dialog::on_roll_button_clicked()
+{
+    ui->roll_button->hide();
+    result_1 = d6->roll();
+    result_2 = d6->roll();
+    dice::show_dice_result(ui->die_result_1, result_1);
+    dice::show_dice_result(ui->die_result_2, result_2);
+    roll_times ++;
+    for (int i = 0 ; i < 6; i++)
+        if (numbers[i] != 0)
+            buttons[i]->setVisible(true);
+}
+
+void exploration_dialog::button_candy(int i)
+{
+    numbers[i] = result_1 == 0 ? result_2 : result_1;
+    if (result_1 == 0) result_2 = 0;
+    else result_1 = 0;
+    ui->pushButton->hide();
+    dice::show_dice_result(blanks[i], numbers[i]);
+    bool flag = true;
+    for (auto p : numbers) if (p == 0) flag = false;
+    if (flag)
+    {
+        int expl_result = 100 * (numbers[0] - numbers[3]) + 10 * (numbers[1] - numbers[4]) + (numbers[2] - numbers[5]);
+        if (expl_result <= 0) done(expl_result);
+        if (game->tool_available(1))
+        {
+            QMessageBox* msg = new QMessageBox(this);
+            msg->setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+            msg->setText("你的探索手杖可以使用，会将本次探索结果变为1。\n你是否想要使用？");
+            msg->setWindowTitle("是否使用探索手杖？");
+            msg->exec();
+            if (msg->result() == 1)
+            {
+                game->use_tool(1);
+                done(1);
+            }
+        }
+        if (game->location_event(location_id) == 3)
+        {
+            QMessageBox* msg = new QMessageBox(this);
+            msg->setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+
+            msg->setText(QString("触发了本地点的事件：好运气，可以至多将探索结果减少10。\n当前探索结果是") + QString::number(expl_result) +
+                         QString("，你是否想要减少到") + QString::number(expl_result - 10 < 0 ? 0 : expl_result - 10) + QString("？"));
+            msg->setWindowTitle("是否使用好运气？");
+            msg->exec();
+            if (msg->result() == 1)
+            {
+                expl_result -= 10;
+                if (expl_result <= 0)
+                {
+                    expl_result = 0;
+                    done(0);
+                }
+            }
+        }
+        if (game->artifact_status(1) == 2 and (location_id == 0 or location_id == 5))
+        {
+            QMessageBox* msg = new QMessageBox(this);
+            msg->setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+
+            msg->setText(QString("你的神器隐士之镜已经激活，可以至多将探索结果减少10。\n当前探索结果是") + QString::number(expl_result) +
+                         QString("，你是否想要减少到") + QString::number(expl_result - 10 < 0 ? 0 : expl_result - 10) + QString("？"));
+            msg->setWindowTitle("是否使用隐士之镜？");
+            msg->exec();
+            if (msg->result() == 1)
+            {
+                expl_result -= 10;
+                if (expl_result <= 0)
+                {
+                    expl_result = 0;
+                    done(0);
+                }
+            }
+        }
+        if (game->artifact_status(4) == 2 and (location_id == 2 or location_id == 3))
+        {
+            QMessageBox* msg = new QMessageBox(this);
+            msg->setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+
+            msg->setText(QString("你的神器预示棱镜已经激活，可以至多将探索结果减少10。\n当前探索结果是") + QString::number(expl_result) +
+                         QString("，你是否想要减少到") + QString::number(expl_result - 10 < 0 ? 0 : expl_result - 10) + QString("？"));
+            msg->setWindowTitle("是否使用预示棱镜？");
+            msg->exec();
+            if (msg->result() == 1)
+            {
+                expl_result -= 10;
+                if (expl_result <= 0)
+                {
+                    expl_result = 0;
+                    done(0);
+                }
+            }
+        }
+        done(expl_result);
+    }
+    else
+    {
+        ui->roll_button->show();
+        ui->die_result_1->hide();
+        ui->die_result_2->hide();
+    }
+}
+
+void exploration_dialog::on_pushButton_clicked() {button_candy(0);}
+void exploration_dialog::on_pushButton_2_clicked() {button_candy(1);}
+void exploration_dialog::on_pushButton_3_clicked() {button_candy(2);}
+void exploration_dialog::on_pushButton_4_clicked() {button_candy(3);}
+void exploration_dialog::on_pushButton_5_clicked() {button_candy(4);}
+void exploration_dialog::on_pushButton_6_clicked() {button_candy(5);}
+
+
--- Utopia-Engine/exploration_dialog.h
+++ Utopia-Engine/exploration_dialog.h
@@ -2,6 +2,12 @@
 #define EXPLORATION_DIALOG_H
 
 #include <QDialog>
+#include <QLabel>
+#include <QPushButton>
+#include <QString>
+#include "dice.h"
+#include "game_controller.h"
+
 
 namespace Ui {
 class exploration_dialog;
@@ -12,11 +18,34 @@ class exploration_dialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit exploration_dialog(QWidget *parent = nullptr);
+    explicit exploration_dialog(game_controller* gm, int id, QWidget *parent = nullptr);
     ~exploration_dialog();
+
+private slots:
+    void on_roll_button_clicked();
+
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
 
 private:
     Ui::exploration_dialog *ui;
+    game_controller* game;
+    dice* d6;
+
+    int numbers[6] = {0};
+    int roll_times = 0;
+    int location_id;
+    int result_1, result_2;
+
+    QLabel* blanks[6]; // 0,1,2 for upper layer, 3,4,5 for lower layer
+    QPushButton* buttons[6]; // same as above
+
+    void button_candy(int); // just a private function that deals homogeneously with button clicks
+
 };
 
 #endif // EXPLORATION_DIALOG_H
