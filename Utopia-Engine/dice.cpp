678b8b43d2fe7f1059d4a57fba25a120105f9ea5
Caz-T <89822413+Caz-T@users.noreply.github.com>
2022-06-03 22:39:26 +0800

    implemented dice

--- Utopia-Engine/dice.cpp
+++ Utopia-Engine/dice.cpp
@@ -1,7 +1,19 @@
 #include "dice.h"
 
 dice::dice(QObject *parent)
-    : QObject{parent}
+    : QObject{parent}, random(QRandomGenerator::securelySeeded())
 {
+}
 
+int dice::roll(int sides)
+{
+    return random.generate() % sides + 1;
+}
+
+void dice::show_dice_result(QLabel* lbl, int result)
+{
+    QString path = ":/dice/";
+    path.append(QChar(result + '0'));
+    lbl->setPixmap(QPixmap(path));
+    lbl->show();
 }
--- Utopia-Engine/dice.h
+++ Utopia-Engine/dice.h
@@ -2,6 +2,8 @@
 #define DICE_H
 
 #include <QObject>
+#include <QRandomGenerator>
+#include <QLabel>
 
 // this class is basically a random number generator.
 
@@ -11,6 +13,10 @@ class dice : public QObject
 public:
     explicit dice(QObject *parent = nullptr);
 
+    QRandomGenerator random;
+
+    int roll(int sides = 6); // actually, in Utopia Engine, all dice are six-sided.
+    static void show_dice_result(QLabel* lbl, int result);
 signals:
 
 };
